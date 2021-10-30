99734d89ba41a1ad17d1a9e572d65fcf13dc3d1d
Artyom Artamonov <artem02artamonov@gmail.com>
2021-10-30 15:06:00 +0300

    Small changes - UI test rewritten

--- SleepWriter/SleepWriterUITests/SleepWriterUITests.swift
+++ SleepWriter/SleepWriterUITests/SleepWriterUITests.swift
@@ -23,12 +23,19 @@ class SleepWriterUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testCreateDream() throws {
-        let sampleTitle = "Test title from UI testing"
-        let sampleDescription = "Test description from UI testing"
-        
+    func testDreamWorkflow() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        try createDream(app)
+        try readDream(app)
+        try deleteDream(app)
+        
+    }
+    
+    private func createDream(_ app: XCUIApplication) throws {
+        let sampleTitle = "Test title"
+        let sampleDescription = "Test description"
         
         let titleTextField = app.scrollViews.otherElements.textFields["Title"]
         let descriptionTextField = app.scrollViews.children(matching: .other).element.children(matching: .other).element.children(matching: .textView).element
@@ -41,13 +48,11 @@ class SleepWriterUITests: XCTestCase {
         
         
         app.buttons["Save"].tap()
+        app.buttons["Save"].tap()
     }
     
-    func testReadDream() throws {
-        let dreamTitle = "Example"
-        
-        let app = XCUIApplication()
-        app.launch()
+    private func readDream(_ app: XCUIApplication) throws {
+        let dreamTitle = "Test title"
         
         app.swipeLeft()
         
@@ -55,15 +60,22 @@ class SleepWriterUITests: XCTestCase {
         XCTAssertTrue(app.staticTexts[dreamTitle].exists)
         // Asserts for information inside dream
         app.scrollViews.containing(.other, identifier:"Vertical scroll bar, 1 page").element.swipeDown(velocity: XCUIGestureVelocity.fast)
-                
     }
     
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    private func deleteDream(_ app: XCUIApplication) throws {
+        let dreamTitle = "Test title"
+        
+        app.scrollViews.otherElements.tables.staticTexts[dreamTitle].press(forDuration: 1)
+        
+        app.buttons["Delete"].tap()
     }
+    
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
